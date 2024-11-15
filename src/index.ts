796409122738272a2d666c4e917ff47e3dec7a2e
arijit <arijitchhatui.me@gmail.com>
2024-11-15 22:58:16 +0530

    fix: create ticket

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ config();
 
 import cors from "cors";
 import express from "express";
+import morgan from "morgan";
+
+import loginRouter from "./auth/auth.controller";
+import ticketRouter from "./tickets/tickets.controller";
+
 
 const app = express();
 
@@ -14,10 +19,14 @@ declare module "express" {
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"))
 
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
+
+app.use(loginRouter, ticketRouter);
+
 
 const port = process.env.PORT;
 app.listen(port, () => {
